0a4c77603fe727a858e2c5bd1e1144802464508f
Pai <pikapai@gmail.com>
2015-10-26 14:27:09 +0800

    [New Function] Search Result UI
    1. Change estimatedRowHeight to avoid flicking when scrolling
    
    2. Adjust text position & cell margin

--- Zuzu/SearchResultTableViewController.swift
+++ Zuzu/SearchResultTableViewController.swift
@@ -162,7 +162,7 @@ class SearchResultTableViewController: UITableViewController {
         NSLog("%@ [[viewDidLoad]]", self)
         
         //Configure cell height
-        tableView.estimatedRowHeight = 500//tableView.rowHeight
+        tableView.estimatedRowHeight = tableView.rowHeight
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.allowsSelection = false
         
