4b7379fb0b7b885d0bd1ae53ee00af8ccd3e8867
Pai <pikapai@gmail.com>
2015-11-04 19:38:32 +0800

    [UI] Advance Filter UI adjustment
    1. Remove extra cells when the table height is smaller than the screen
    
    2. Adjust separator width

--- Zuzu/FilterOptionTableViewController.swift
+++ Zuzu/FilterOptionTableViewController.swift
@@ -15,11 +15,8 @@ class FilterOptionTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        //Remove extra cells when the table height is smaller than the screen
+        tableView.tableFooterView = UIView(frame: CGRectZero)
     }
 
     override func didReceiveMemoryWarning() {
