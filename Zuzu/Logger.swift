43dfa8b737216bb6b04ef237ba8c313862e9c6fe
Jung-Shuo Pai <pikapai@gmail.com>
2015-12-02 14:49:03 +0800

    [Util] Add a file logger that output logs to a file with timestamped name

--- Zuzu/Logger.swift
+++ Zuzu/Logger.swift
@@ -12,10 +12,19 @@ import XCGLogger
 public struct Logger {
     
     static let defaultLogger = Logger.createDefaultlogger()
- 
+    
+    static let fileLogger = Logger.createFilelogger()
+    
+    private static let logFileIdentifier = "com.lap.zuzu"
+    
+    private static let dateFormatter: NSDateFormatter = {
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "yyyyMMdd'T'HHmmssZ"
+        return formatter
+    }()
     
     private static func createDefaultlogger() -> XCGLogger{
-    
+        
         let logger = XCGLogger.defaultInstance()
         
         #if DEBUG
@@ -25,6 +34,45 @@ public struct Logger {
         #endif
         
         return logger
+        
+    }
     
+    private static func createFilelogger() -> XCGLogger{
+        
+        let logger = XCGLogger()
+        
+        #if DEBUG
+            logger.setup(.Debug, showThreadName: true, showLogLevel: false, showFileNames: true, showLineNumbers: true)
+        #else
+            logger.setup(.Severe, showThreadName: true, showLogLevel: false, showFileNames: true, showLineNumbers: false)
+        #endif
+        
+        if let dirPath = getLogDirPath() {
+            
+            let logFilePath = "\(dirPath)/\(dateFormatter.stringFromDate(NSDate()))"
+            
+            logger.addLogDestination(XCGFileLogDestination(owner: logger, writeToFile: logFilePath, identifier: logFileIdentifier))
+        }
+        
+        return logger
+        
+    }
+    
+    private static func getLogDirPath() -> String? {
+        if let cacheDir = NSSearchPathForDirectoriesInDomains(.CachesDirectory, .UserDomainMask, true).first {
+            let logDir = "\(cacheDir)/Logs"
+            if !NSFileManager.defaultManager().fileExistsAtPath(logDir) {
+                do {
+                    try NSFileManager.defaultManager().createDirectoryAtPath(logDir, withIntermediateDirectories: false, attributes: nil)
+                    return logDir
+                } catch _ as NSError {
+                    return nil
+                }
+            } else {
+                return logDir
+            }
+        }
+        
+        return nil
     }
 }
