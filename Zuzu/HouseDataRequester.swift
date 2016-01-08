05f6bfeed9aa98cc490d6e8b94b23171cd662705
Jung-Shuo Pai <pikapai@gmail.com>
2016-01-08 16:35:50 +0800

    [Refactor] Put secrets in another file which is not pushed to Git server

--- Zuzu/HouseDataRequester.swift
+++ Zuzu/HouseDataRequester.swift
@@ -492,7 +492,7 @@ public class HouseDataRequester: NSObject, NSURLConnectionDelegate {
             
             request.HTTPMethod = SolrConst.Server.HTTP_METHOD
             
-            let plainLoginString = (SolrConst.SolrSecret as NSString).dataUsingEncoding(NSUTF8StringEncoding)
+            let plainLoginString = (SecretConst.SolrQuery as NSString).dataUsingEncoding(NSUTF8StringEncoding)
             
             if let base64LoginString = plainLoginString?.base64EncodedStringWithOptions(NSDataBase64EncodingOptions.Encoding64CharacterLineLength) {
                 
