1616c4ca5af6b1d5c6a550cda428de2cd49fd60d
Harry Yeh <eechih@gmail.com>
2016-02-02 16:30:32 +0800

    [Enhancement] New ZuzuWebService method: createUser()

--- Zuzu/ZuzuWebService.swift
+++ Zuzu/ZuzuWebService.swift
@@ -15,7 +15,7 @@ struct WebApiConst {
     
     struct Server {
         static let SCHEME = "http"
-        static let HOST = "127.0.0.1" //"192.168.100.23" //"192.168.1.241"
+        static let HOST = "127.0.0.1" // "127.0.0.1" , "192.168.100.23", "192.168.1.241"
         static let PORT = 4567
     }
     
@@ -96,6 +96,34 @@ class ZuzuWebService: NSObject
             }
         }
 
+        Log.exit()
+    }
+    
+    func createUser(user: ZuzuUser, handler: (result: String?, error: ErrorType?) -> Void) {
+        Log.enter()
+        
+        let url = "\(self.hostUrl)/user"
+        let payload = Mapper<ZuzuUser>().toJSON(user)
+        
+        Alamofire.request(.POST, url, parameters: [:], encoding: .Custom({
+            (convertible, params) in
+            let mutableRequest = convertible.URLRequest.copy() as! NSMutableURLRequest
+            mutableRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            mutableRequest.HTTPBody = try! NSJSONSerialization.dataWithJSONObject(payload, options: [])
+            return (mutableRequest, nil)
+        })).responseString { (_, _, result) in
+            Log.debug("URL: \(url)")
+            
+            switch result {
+            case .Success(let value):
+                Log.debug("Success.")
+                handler(result: value, error: nil)
+            case .Failure(_, let error):
+                Log.debug("Error: \(error)")
+                handler(result: nil, error: error)
+            }
+        }
+        
         Log.exit()
     }
 
