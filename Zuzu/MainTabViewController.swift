52aad109ba9d1cfe85df84b29d341adc3770e231
ted-lee <alight.lee@gmail.com>
2016-02-03 00:59:14 +0800

    Alert FB login before entering radar settings

--- Zuzu/MainTabViewController.swift
+++ Zuzu/MainTabViewController.swift
@@ -75,6 +75,8 @@ class MainTabViewController: UITabBarController, UITabBarControllerDelegate {
             if let name: String = sb.valueForKey("name") as? String {
                 switch name {
                 case "MyCollectionStoryboard":
+                    fallthrough
+                case "RadarStoryboard":
                     if !AmazonClientManager.sharedInstance.isLoggedIn() {
                         AmazonClientManager.sharedInstance.loginFromView(self) {
                             (task: AWSTask!) -> AnyObject! in
