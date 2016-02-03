fe8aec1b1c40239bc5f581d6163c97d3969e6521
ted-lee <alight.lee@gmail.com>
2016-02-03 17:25:07 +0800

    [APP notification] Mobile token management with Amazon SNS

--- Zuzu/AmazonClientManager.swift
+++ Zuzu/AmazonClientManager.swift
@@ -51,6 +51,7 @@ class AmazonClientManager : NSObject {
         didSet {
             if let userData = self.fbLoginData {
                 self.uploadFBUserDataToS3(userData)
+                self.registerSNSEndpoint()
             }
         }
     }
@@ -146,8 +147,6 @@ class AmazonClientManager : NSObject {
             self.dumpCredentialProviderInfo()
 
             if (task.error != nil) {
-                
-                
                 let userDefaults = NSUserDefaults.standardUserDefaults()
                 let currentDeviceToken: NSData? = userDefaults.objectForKey(Constants.DEVICE_TOKEN_KEY) as? NSData
                 var currentDeviceTokenString : String
@@ -363,12 +362,96 @@ class AmazonClientManager : NSObject {
     }
     
     // MARK: SNS Push Notifications
-    
+
     func registerSNSEndpoint(){
+        let userData = self.fbLoginData?.facebookEmail
+        let endpointArn = UserDefaultsUtils.getSNSEndpointArn()
+        if endpointArn == nil && userData == nil{
+            return
+        }
+        
         if let deviceTokenString = UserDefaultsUtils.getAPNDevicetoken(){
+            if endpointArn != nil{
+                let sns = AWSSNS.defaultSNS()
+                let request = AWSSNSGetEndpointAttributesInput()
+                request.endpointArn = endpointArn
+                sns.getEndpointAttributes(request).continueWithExecutor(AWSExecutor.mainThreadExecutor(), withBlock: { (task: AWSTask!) -> AnyObject! in
+                    if task.error != nil {
+                        Log.debug("Error: \(task.error)")
+                        if task.error!.code == AWSSNSErrorType.NotFound.rawValue{
+                            self.createEndpoint(deviceTokenString, userData:userData)
+                        }
+                    } else {
+                        let getEndpointResponse = task.result as! AWSSNSGetEndpointAttributesResponse
+                        Log.debug("token: \(getEndpointResponse.attributes!["Token"])")
+                        Log.debug("Enabled: \(getEndpointResponse.attributes!["Enabled"])")
+                        Log.debug("CustomUserData: \(getEndpointResponse.attributes!["CustomUserData"])")
+                        var isUpdate = false
+                        if let lastToken = getEndpointResponse.attributes!["Token"] as? String{
+                            if deviceTokenString != lastToken{
+                                isUpdate = true
+                            }
+                        }
+                        
+                        if let enabled = getEndpointResponse.attributes!["Enabled"] as? Bool{
+                            if enabled == false{
+                                isUpdate = true
+                            }
+                        }
+                        
+                        var customUserData = getEndpointResponse.attributes!["CustomUserData"] as? String
+                        if customUserData == nil{
+                            if userData != nil{
+                                customUserData = userData
+                                isUpdate = true
+                            }
+                        }
+                        
+                        if isUpdate == true{
+                            self.updateEndpoint(deviceTokenString, endpointArn:endpointArn!, userData: customUserData)
+                        }
+                    }
+                    
+                    return nil
+                })
+            }else{
+                self.createEndpoint(deviceTokenString, userData: userData)
+            }
+        }
+    }
+    
+    func updateEndpoint(deviceTokenString: String, endpointArn:String, userData: String?){
+        let sns = AWSSNS.defaultSNS()
+        let request = AWSSNSSetEndpointAttributesInput()
+        Log.debug("endpointArn: \(request.endpointArn)")
+        request.endpointArn = endpointArn
+        request.attributes = [NSObject:AnyObject]()
+        request.attributes!["Token"] = deviceTokenString
+        request.attributes!["Enabled"] = "true"
+        if let customUserData = userData{
+            request.attributes!["CustomUserData"] = customUserData
+        }
+        Log.debug("endpointArn: \(request.endpointArn)")
+        Log.debug("Token: \(request.attributes!["Token"])")
+        Log.debug("Enabled: \(request.attributes!["Enabled"])")
+        sns.setEndpointAttributes(request).continueWithExecutor(AWSExecutor.mainThreadExecutor(), withBlock: { (task: AWSTask!) -> AnyObject! in
+            if task.error != nil {
+                Log.debug("Error: \(task.error)")
+            } else {
+                Log.debug("update SNS endpoint successful")
+            }
+            
+            return nil
+        })
+
+    }
+    
+    func createEndpoint(deviceTokenString: String, userData: String?){
+        if let customUserData = userData{
             let sns = AWSSNS.defaultSNS()
             let request = AWSSNSCreatePlatformEndpointInput()
             request.token = deviceTokenString
+            request.customUserData = customUserData
             request.platformApplicationArn = AWSConstants.PLATFORM_APPLICATION_ARN
             sns.createPlatformEndpoint(request).continueWithExecutor(AWSExecutor.mainThreadExecutor(), withBlock: { (task: AWSTask!) -> AnyObject! in
                 if task.error != nil {
@@ -376,7 +459,9 @@ class AmazonClientManager : NSObject {
                 } else {
                     let createEndpointResponse = task.result as! AWSSNSCreateEndpointResponse
                     Log.debug("endpointArn: \(createEndpointResponse.endpointArn)")
-                    NSUserDefaults.standardUserDefaults().setObject(createEndpointResponse.endpointArn, forKey: "endpointArn")
+                    if let endpointArn = createEndpointResponse.endpointArn{
+                        UserDefaultsUtils.setSNSEndpointArn(endpointArn)
+                    }
                 }
                 
                 return nil
@@ -384,13 +469,14 @@ class AmazonClientManager : NSObject {
         }
     }
     
+    
     // MARK: S3
     
     //func uploadFBUserDataToS3(transferManager: AWSS3TransferManager, fbLoginData: FBUserData) {
     
     func uploadFBUserDataToS3(userData: FBUserData) {
         Log.debug("\(self) uploadFBUserDataToS3")
-        
+
         let S3UploadKeyName = userData.facebookId! + ".json"
         
         //Create a test file in the temporary directory
