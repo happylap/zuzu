6a8affe239a7a08b425e5d4985fe2c909b1abde4
Jung-Shuo Pai <pikapai@gmail.com>
2016-02-16 16:46:16 +0800

    Refactor for Google Sign-In

---
+++ Zuzu/UserData.swift
@@ -0,0 +1,49 @@
+//
+//  UserInfo.swift
+//  Zuzu
+//
+//  Created by eechih on 12/30/15.
+//  Copyright © 2015 Jung-Shuo Pai. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+enum Provider: String {
+    case FB, GOOGLE
+}
+
+class UserData: NSObject, Mappable {
+    
+    var provider: Provider?
+    var id: String?
+    var email: String?
+    var name: String?
+    var gender: String?
+    var birthday: String?
+    var pictureUrl: String?
+    
+    init(provider: Provider) {
+        self.provider = provider
+    }
+    
+    required init?(_ map: Map) {
+
+    }
+    
+    // Mappable
+    func mapping(map: Map) {
+        provider          <- map["provider"]
+        id          <- map["id"]
+        email       <- map["email"]
+        name   <- map["name"]
+        gender      <- map["gender"]
+        birthday    <- (map["birthday"])
+        pictureUrl    <- (map["pictureUrl"])
+    }
+    
+    override var description: String {
+        let string = "UserData: id = \(id)\n email = \(email)\n name = \(name)\n gender = \(gender)\n birthday = \(birthday)\n pictureUrl = \(pictureUrl)"
+        return string
+    }
+}
