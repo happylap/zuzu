fc7fba575bf1993df1bbc5f575924fb469f56485
ted-lee <alight.lee@gmail.com>
2016-03-13 22:51:18 +0800

    遇到這個 access token 驗證問題時, 點進去雷達，畫面會一片空白

--- Zuzu/RadarNavigationController.swift
+++ Zuzu/RadarNavigationController.swift
@@ -105,13 +105,6 @@ class RadarNavigationController: UINavigationController {
     }
     
     func showRetryRadarView(isBlank: Bool){
-        if self.viewControllers.count > 0 {
-            if let vc = self.viewControllers[0] as? RadarRetryViewController{
-                vc.isBlank = isBlank
-                return
-            }
-        }
-
         let storyboard = UIStoryboard(name: "RadarStoryboard", bundle: nil)
         if let vc = storyboard.instantiateViewControllerWithIdentifier("RadarRetryViewController") as? RadarRetryViewController {
             vc.isBlank = isBlank
