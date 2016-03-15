c146c5a2f424f624d5fc9207b14aa16814f0a62e
Jung-Shuo Pai <pikapai@gmail.com>
2016-03-15 10:23:09 +0800

    [App] Radar Landing Page
    Make images circles

--- Zuzu/RadarLandingViewController.swift
+++ Zuzu/RadarLandingViewController.swift
@@ -9,6 +9,30 @@ import UIKit
 
 class RadarLandingViewController: UIViewController {
     
+    @IBOutlet weak var leftSampleImage: UIImageView! {
+        didSet {
+            
+            leftSampleImage.layer.borderWidth=1.0
+            leftSampleImage.layer.masksToBounds = false
+            leftSampleImage.layer.borderColor = UIColor.lightGrayColor().CGColor
+            leftSampleImage.layer.cornerRadius = 13
+            leftSampleImage.layer.cornerRadius = leftSampleImage.frame.size.height/2
+            leftSampleImage.clipsToBounds = true
+        }
+    }
+    
+    @IBOutlet weak var rightSampleImage: UIImageView!{
+        didSet {
+            
+            rightSampleImage.layer.borderWidth=1.0
+            rightSampleImage.layer.masksToBounds = false
+            rightSampleImage.layer.borderColor = UIColor.lightGrayColor().CGColor
+            rightSampleImage.layer.cornerRadius = 13
+            rightSampleImage.layer.cornerRadius = rightSampleImage.frame.size.height/2
+            rightSampleImage.clipsToBounds = true
+        }
+    }
+    
     @IBOutlet weak var continueButton: UIButton! {
         didSet {
             continueButton.backgroundColor = UIColor.colorWithRGB(0x1CD4C6, alpha: 1)
