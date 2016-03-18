051135f2a9f895bab90a307ca8ea137d4c9d1fdd
Jung-Shuo Pai <pikapai@gmail.com>
2016-03-18 23:50:45 +0800

    [Refactor] Refactor Global Constants

--- Zuzu/Contants.swift
+++ Zuzu/Contants.swift
@@ -9,29 +9,58 @@
 import Foundation
 import AWSCore
 
+// MARK: My Collection
+struct CollectionConstants {
 
-struct Constants {
-    
-    // MARK: My Collection
     static let MYCOLLECTION_MAX_SIZE = 60
     
-    // MARK: Optional: Enable Facebook Login
+}
+
+// MARK: Constants for TagManager
+struct TagConst {
+    
+    static let showADs = "showADs"
+    
+}
+
+// MARK: Constants for Criteria
+struct CriteriaConst {
     
     /**
-    * OPTIONAL: Enable FB Login
-    *
-    * To enable FB Login
-    * 1. Add FacebookAppID in App plist file
-    * 2. Add the appropriate URL handler in project (should match FacebookAppID)
-    */
+     "整層住家":0
+     "獨立套房":1
+     "分租套房":2
+     "雅房":3
+     "店面":4
+     "攤位":5
+     "辦公":6
+     "住辦":7
+     **/
     
+    struct PrimaryType {
+        static let FULL_FLOOR = 1
+        static let SUITE_INDEPENDENT = 2
+        static let SUITE_COMMON_AREA = 3
+        static let ROOM_NO_TOILET = 4
+        static let HOME_OFFICE = 8
+    }
     
-    /*******************************************
-     * DO NOT CHANGE THE VALUES BELOW HERE
-     */
-        
-    static let DEVICE_TOKEN_KEY = "DeviceToken"
-    static let COGNITO_DEVICE_TOKEN_KEY = "CognitoDeviceToken"
-    static let COGNITO_PUSH_NOTIF = "CognitoPushNotification"
+    /**
+     "公寓": 1
+     "電梯大樓": 2
+     "透天厝": 3
+     "別墅": 4
+     **/
     
+    struct HouseType {
+        static let BUILDING_WITHOUT_ELEVATOR = 1
+        static let BUILDING_WITH_ELEVATOR = 2
+        static let INDEPENDENT_HOUSE = 3
+        static let INDEPENDENT_HOUSE_WITH_GARDEN = 4
+    }
+    
+    struct Bound {
+        static let LOWER_ANY = -1
+        static let UPPER_ANY = -1
+    }
 }
