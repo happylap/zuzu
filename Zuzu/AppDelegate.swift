4199f0098ccab3cf175780cff34abda900842fba
Jung-Shuo Pai <pikapai@gmail.com>
2016-03-21 22:26:18 +0800

    [Add] Installation tracking for FB ADs

--- Zuzu/AppDelegate.swift
+++ Zuzu/AppDelegate.swift
@@ -313,6 +313,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
+        #if !DEBUG
+            /// Installation tracking for FB ADs
+            FBSDKAppEvents.activateApp()
+        #endif
+
     }
     
     func applicationWillTerminate(application: UIApplication) {
