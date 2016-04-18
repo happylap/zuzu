b87808dd77b6a0671ac25bdbe30c81bc482731e6
Jung-Shuo Pai <pikapai@gmail.com>
2016-04-18 17:05:51 +0800

    忘記密碼UI Layout http://nordland.oriconsys.com/issues/651
    - Add view controller class

---
+++ Zuzu/ForgotPasswordViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ForgotPasswordViewController.swift
+//  Zuzu
+//
+//Copyright © LAP Inc. All rights reserved
+//
+
+import UIKit
+
+class ForgotPasswordViewController: UIViewController {
+
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
