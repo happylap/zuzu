0f3bb7721ad361cd1dcf9543bd3211d6693846ce
Jung-Shuo Pai <pikapai@gmail.com>
2016-04-26 11:48:44 +0800

    [Refactor]
    Add comment for different app life cycle callback

--- Zuzu/AppDelegate.swift
+++ Zuzu/AppDelegate.swift
@@ -313,7 +313,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
     }
     
-    // MARK: UIApplicationDelegate App Life Cycle
+    // MARK: UIApplicationDelegate: App Life Cycle
+    // MARK: App Life Cycle - didFinishLaunchingWithOptions
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
         Log.enter()
@@ -365,18 +366,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
+    // MARK: App Life Cycle - applicationWillResignActive
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
         Log.enter()
     }
     
+    // MARK: App Life Cycle - applicationDidEnterBackground
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
         Log.enter()
     }
     
+    // MARK: App Life Cycle - applicationWillEnterForeground
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
         
@@ -403,7 +407,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         /// Try to trigger the timer for refreshing token
         AmazonClientManager.sharedInstance.triggerTokenRefreshingTimer()
     }
-    
+
+    // MARK: App Life Cycle - applicationDidBecomeActive
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         
@@ -416,6 +421,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
     }
     
+    // MARK: App Life Cycle - Terminate
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         Log.enter()
