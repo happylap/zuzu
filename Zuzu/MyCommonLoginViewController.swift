971abf0dbace4077ce632e56c0dd5eb092e4cda3
Jung-Shuo Pai <pikapai@gmail.com>
2016-04-26 17:17:51 +0800

    Full Screen login page http://nordland.oriconsys.com/issues/594
    - Overall UI layout

--- Zuzu/MyCommonLoginViewController.swift
+++ Zuzu/MyCommonLoginViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 private let Log = Logger.defaultLogger
 
-protocol CommonLoginViewDelegate {
+protocol MyCommonLoginViewControllerDelegate {
     
     func onPerformSocialLogin(provider: Provider)
     
@@ -19,7 +19,7 @@ protocol CommonLoginViewDelegate {
     
 }
 
-class CommonLoginViewController: UIViewController {
+class MyCommonLoginViewController: UIViewController {
     
     // segue to configure UI
     
@@ -37,7 +37,7 @@ class CommonLoginViewController: UIViewController {
     @IBOutlet weak var cancelButton: UIButton!{
         didSet {
             cancelButton.setImage(UIImage(named: "cancel")?.imageWithRenderingMode(.AlwaysTemplate), forState: UIControlState.Normal)
-            cancelButton.tintColor = UIColor.colorWithRGB(0xD4D4D4, alpha: 1)
+            cancelButton.tintColor = UIColor.whiteColor()
             
             cancelButton.addTarget(self, action: #selector(CommonLoginViewController.onCancelButtonTouched(_:)), forControlEvents: UIControlEvents.TouchDown)
         }
@@ -46,11 +46,12 @@ class CommonLoginViewController: UIViewController {
     @IBOutlet weak var userRegisterButton: UIButton! {
         
         didSet {
+            //userRegisterButton.backgroundColor = UIColor.colorWithRGB(0x1CD4C6, alpha: 1)
             userRegisterButton.layer.borderWidth = 2
-            userRegisterButton.layer.borderColor = UIColor.colorWithRGB(0x00B4C6, alpha: 1).CGColor
-            userRegisterButton.backgroundColor = UIColor.colorWithRGB(0x00B4C6, alpha: 1)
-            
-            userRegisterButton.tintColor = UIColor.colorWithRGB(0xFFFFFF, alpha: 1)
+            userRegisterButton.layer.borderColor =
+                UIColor.colorWithRGB(0x00CD96, alpha: 1).CGColor
+            userRegisterButton.tintColor =
+                UIColor.colorWithRGB(0x00CD96, alpha: 1)
             
             userRegisterButton.addTarget(self, action: #selector(CommonLoginViewController.onZuzuRegisterButtonTouched(_:)), forControlEvents: UIControlEvents.TouchDown)
         }
@@ -60,10 +61,12 @@ class CommonLoginViewController: UIViewController {
     @IBOutlet weak var userLoginButton: UIButton! {
         
         didSet {
+            //userLoginButton.backgroundColor = UIColor.colorWithRGB(0x1CD4C6, alpha: 1)
             userLoginButton.layer.borderWidth = 2
-            userLoginButton.layer.borderColor = UIColor.colorWithRGB(0x00B4C6, alpha: 1).CGColor
-            userLoginButton.backgroundColor = UIColor.colorWithRGB(0x00B4C6, alpha: 1)
-            userLoginButton.tintColor = UIColor.colorWithRGB(0xFFFFFF, alpha: 1)
+            userLoginButton.layer.borderColor =
+                UIColor.colorWithRGB(0x00CD96, alpha: 1).CGColor
+            userLoginButton.tintColor =
+                UIColor.colorWithRGB(0x00CD96, alpha: 1)
             
             userLoginButton.addTarget(self, action: #selector(CommonLoginViewController.onZuzuLoginButtonTouched(_:)), forControlEvents: UIControlEvents.TouchDown)
         }
@@ -91,7 +94,7 @@ class CommonLoginViewController: UIViewController {
                 let thickness:CGFloat = 0.5
                 
                 let upperBorder = CALayer()
-                upperBorder.backgroundColor = UIColor.whiteColor().CGColor
+                upperBorder.backgroundColor = UIColor.lightGrayColor().CGColor
                 
                 upperBorder.frame = CGRect(x: 0, y: 0, width: customLoginView.frame.width, height: thickness)
                 
@@ -100,15 +103,20 @@ class CommonLoginViewController: UIViewController {
             } else {
                 customLoginView.hidden = true
                 customLoginView.removeFromSuperview()
-                
+
             }
         }
         
     }
+    //@IBOutlet weak var maskView: UIView!
     
-    @IBOutlet weak var privacyIconView: UIImageView! {
+    @IBOutlet weak var titleText: UILabel!
+    
+    @IBOutlet weak var subTitleText: UILabel!
+    
+    @IBOutlet weak var faceImageView: UIImageView! {
         didSet{
-            privacyIconView.image = UIImage(named: "login_privacy_icon")?.imageWithRenderingMode(.AlwaysTemplate)
+            faceImageView.image = UIImage(named: "lock_icon")?.imageWithRenderingMode(.AlwaysTemplate)
         }
     }
     
@@ -116,15 +124,20 @@ class CommonLoginViewController: UIViewController {
         didSet{
             
             fbButton.layer.borderWidth = 2
-            fbButton.layer.borderColor = UIColor.colorWithRGB(0x4990E2, alpha: 1).CGColor
+            fbButton.layer.borderColor =
+                UIColor.colorWithRGB(0x4990E2, alpha: 1).CGColor
             
             fbButton.setImage(UIImage(named: "facebook_icon")?.imageWithRenderingMode(.AlwaysTemplate), forState: UIControlState.Normal)
             fbButton.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
-            fbButton.imageEdgeInsets = UIEdgeInsetsMake(0, -6, 0, 0)
-            fbButton.tintColor = UIColor.colorWithRGB(0x4990E2, alpha: 1)
             
-            fbButton.backgroundColor = UIColor.colorWithRGB(0x4990E2, alpha: 1)
-            fbButton.setTitleColor(UIColor.colorWithRGB(0xFFFFFF, alpha: 1), forState: .Normal)
+            fbButton.imageEdgeInsets = UIEdgeInsetsMake(0, -6, 0, 0)
+            
+            fbButton.tintColor =
+                UIColor.colorWithRGB(0x4990E2, alpha: 1)
+            
+            //fbButton.backgroundColor = UIColor.colorWithRGB(0x1CD4C6, alpha: 1)
+            fbButton.setTitleColor(UIColor.colorWithRGB(0x4990E2, alpha: 1), forState: .Normal)
+            fbButton.layer.cornerRadius = CGFloat(5.0)
             
             fbButton.addTarget(self, action: #selector(CommonLoginViewController.onFBButtonTouched(_:)), forControlEvents: UIControlEvents.TouchDown)
         }
@@ -134,15 +147,19 @@ class CommonLoginViewController: UIViewController {
         didSet{
             
             googleButton.layer.borderWidth = 2
-            googleButton.layer.borderColor = UIColor.colorWithRGB(0xF3364C, alpha: 1).CGColor
+            googleButton.layer.borderColor =
+                UIColor.colorWithRGB(0xF3364C, alpha: 1).CGColor
             
             googleButton.setImage(UIImage(named: "google_icon")?.imageWithRenderingMode(.AlwaysTemplate), forState: UIControlState.Normal)
             googleButton.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
             googleButton.imageEdgeInsets = UIEdgeInsetsMake(0, 0, 0, 0)
-            googleButton.tintColor = UIColor.colorWithRGB(0xF3364C, alpha: 1)
             
-            googleButton.backgroundColor = UIColor.colorWithRGB(0xF3364C, alpha: 1)
-            googleButton.setTitleColor(UIColor.colorWithRGB(0xFFFFFF, alpha: 1), forState: .Normal)
+            googleButton.tintColor =
+                UIColor.colorWithRGB(0xF3364C, alpha: 1)
+            
+            //googleButton.backgroundColor = UIColor.colorWithRGB(0x1CD4C6, alpha: 1)
+            googleButton.setTitleColor(UIColor.colorWithRGB(0xF3364C, alpha: 1), forState: .Normal)
+            googleButton.layer.cornerRadius = CGFloat(5.0)
             
             googleButton.addTarget(self, action: #selector(CommonLoginViewController.onGoogleButtonTouched(_:)), forControlEvents: UIControlEvents.TouchDown)
         }
@@ -205,6 +222,20 @@ class CommonLoginViewController: UIViewController {
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
+        if loginMode == 1 {
+            titleText.text = "登入使用我的收藏"
+            subTitleText.text = "請登入使用「我的收藏」，收藏的物件將會被儲存在雲端，日後更換裝置也不會不見喔！"
+        } else if loginMode == 2 {
+            titleText.text = "登入使用租屋雷達"
+            subTitleText.text = "請您使用下列帳號登入，豬豬快租便能為您提供個人專屬的通知服務"
+        } else if loginMode == 3 {
+            titleText.text = "登入啟用租屋雷達服務"
+            subTitleText.text = "我們發現您有購買的租屋雷達尚未完成啟用，請您使用下列帳號登入啟用服務，以維護您的權益"
+        }
+        
+        //        self.maskView.alpha = 0.0
+        //        self.maskView.hidden = false
+        
         if let tabBar = self.presentingViewController?.tabBarController{
             self.isOriginallyHideTabBar = tabBar.tabBarHidden
             
@@ -216,6 +247,10 @@ class CommonLoginViewController: UIViewController {
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
+        
+        //        UIView.animateWithDuration(0.5, animations: {
+        //            self.maskView.alpha = 0.3
+        //        })
     }
     
     override func viewWillDisappear(animated: Bool) {
