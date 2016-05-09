4c885d5a8daec03971464e4f3e2987e3df1cb052
Jung-Shuo Pai <pikapai@gmail.com>
2016-05-09 16:15:58 +0800

    允許使用者未登入購買租屋雷達
    - Backward compatibility 1.0 -> 1.1

--- Zuzu/UserDefaultsUtils.swift
+++ Zuzu/UserDefaultsUtils.swift
@@ -220,6 +220,7 @@ struct UserDefaultsUtils {
         if let userProfile = UserDefaultsUtils.getUserProfile(), userId = userProfile.id  {
             
             UserDefaultsUtils.setLoginUser(userId)
+            UserDefaultsUtils.clearUserProfile()
             
         }
         
