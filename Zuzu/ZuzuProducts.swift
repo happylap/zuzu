ecdd0be3848ef439117c57c3a166537a963ef86b
Jung-Shuo Pai <pikapai@gmail.com>
2016-06-02 16:54:08 +0800

    http://nordland.oriconsys.com/issues/778   雷達Store: 新增長期找屋方案 120元

--- Zuzu/ZuzuProducts.swift
+++ Zuzu/ZuzuProducts.swift
@@ -13,11 +13,13 @@ public struct ZuzuProducts {
     private static let Prefix = "com.lap.zuzurentals."
     
     /// MARK: - Supported Product Identifiers
-    public static let ProductRadar1Month = Prefix + "radar1"
+    public static let ProductRadar10 = Prefix + "radar1"
     
-    public static let ProductRadar2Month = Prefix + "radar2"
+    public static let ProductRadar30 = Prefix + "radar2"
     
-    public static let ProductRadar3Month = Prefix + "radar3"
+    public static let ProductRadar45 = Prefix + "radar3"
+    
+    public static let ProductRadar60 = Prefix + "radar4"
     
     public static let ProductRadarFreeTrial = Prefix + "radarfree1"
     
@@ -35,7 +37,8 @@ public struct ZuzuProducts {
     
     // All of the products assembled into a set of product identifiers.
     public static let productIdentifiers: Set<ProductIdentifier> =
-    [ZuzuProducts.ProductRadarFreeTrial, ZuzuProducts.ProductRadar1Month, ZuzuProducts.ProductRadar2Month, ZuzuProducts.ProductRadar3Month]
+    [ZuzuProducts.ProductRadarFreeTrial,
+     ZuzuProducts.ProductRadar10, ZuzuProducts.ProductRadar30, ZuzuProducts.ProductRadar45, ZuzuProducts.ProductRadar60]
     
     /// Return the resourcename for the product identifier.
     internal static func resourceNameForProductIdentifier(productIdentifier: String) -> String? {
