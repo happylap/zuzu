08788b6207aecf93d5d10510a515e9de629d18c6
ted-lee <alight.lee@gmail.com>
2016-06-06 16:49:01 +0800

    雷達購買後的啟用與狀態通知

--- Zuzu/RadarDisplayViewController.swift
+++ Zuzu/RadarDisplayViewController.swift
@@ -776,9 +776,21 @@ extension RadarDisplayViewController: RadarPurchaseDelegate{
     
     func onPurchaseSuccess() -> Void{
         Log.enter()
+        
         UserServiceStatusManager.shared.resetServiceStatusCache() // reset service cache
         
         self.enableCriteriaForPurchase()
+        
+        if let userId = UserManager.getCurrentUser()?.userId, endpointArn = UserDefaultsUtils.getSNSEndpointArn(){
+            // calling the sendNotifcaiont is just to make sure the app can receive the push notification from server
+            // we don't want it to engage the flow about purchase and criteria setup, thus, we delay the sending for 5 seconds
+            self.runOnMainThreadAfter(5.0){
+                ZuzuWebService.sharedInstance.sendNotification(userId, targetARN: endpointArn, customMessage: nil, handler: {
+                    (result, error) in
+                    // don't need to handle the error
+                })
+            }
+        }
         
         Log.exit()
     }
