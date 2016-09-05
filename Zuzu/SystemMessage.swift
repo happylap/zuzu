82846f1cf5d04b4e13e3415859e338bb71b7020e
Jung-Shuo Pai <pikapai@gmail.com>
2016-09-05 17:40:38 +0800

    Task #1035 調整 UI 行為，點擊物件時，要載入對方網頁時，會說: 即將離開豬豬快租，開啟原始房源網頁

--- Zuzu/SystemMessage.swift
+++ Zuzu/SystemMessage.swift
@@ -17,6 +17,8 @@ struct SystemMessage {
         static let EMPTY_HISTORICAL_SEARCH = "尚無任何\"搜尋紀錄\"\n\n日後任何的搜尋紀錄，都會記錄在這邊，方便查找"
         static let EMPTY_HISTORICAL_PURCHASE = "尚無任何\"購買紀錄\"\n\n不妨嘗試在租屋雷達頁，設定並取符合需求的通知條件"
         static let EMPTY_SEARCH_RESULT = "喔喔...目前的搜尋條件找不到任何物件\n\n嘗試換個條件再搜尋一次！"
+        
+        static let EXTERNAL_SITE_RESULT = "即將離開豬豬快租\n\n開啟原始房源網頁"
     }
 
     struct ALERT {
